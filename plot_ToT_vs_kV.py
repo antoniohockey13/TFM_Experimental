080209103fb66b407252b42fb154f6f2325c9478
antoniohockey13 <antoniogomezcarrera@gmail.com>
2024-05-29 13:59:13 +0200

    Added mean mode and median WPRK more on MEDIAN

--- plot_ToT_vs_kV.py
+++ plot_ToT_vs_kV.py
@@ -1,6 +1,8 @@
 import ROOT
 import click
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 # CONSTANTS
 FORMAT = ".png"
@@ -25,11 +27,18 @@ def main(inputfiles):
         folder = "/".join(input_name[1:-1])
         os.makedirs(f"Pictures/{folder}", exist_ok=True)
     
+    mean = {}
+    median = {}
+    mode = {}
+    voltages_kV = [10,15,20,35,30,35]    
+
     for irow in range(6,10):
         c = ROOT.TCanvas(f"c{irow}", f"c{irow}")
         legend = ROOT.TLegend(0.1,0.7,0.48,0.9)
         tot_calibrated = []
-        for i, inputfile in enumerate(inputfiles):
+
+        
+        for i, inputfile in enumerate(reversed(inputfiles)):
             file = ROOT.TFile(inputfile)
             max_bin_value = get_cal(file, f"row=={irow}")
             if i == 0:
@@ -38,9 +47,14 @@ def main(inputfiles):
                 opt = "same"
             file.Hits.SetLineColor(i+1)
             # Change limits
-            tot_calibrated.append(ROOT.TH1F(f"tot_calibrated_{i}", "", 250, 0, 25)) 
+            tot_calibrated.append(ROOT.TH1F(f"tot_calibrated_{i}", "", 100, 0, 10)) 
             file.Hits.Project(f"tot_calibrated_{i}", "tot_cal", f"row == {irow} && cal-{max_bin_value}<2.5")
             tot_calibrated[-1].SetDirectory(0) 
+            # Statistics values
+            mean[irow] = tot_calibrated[-1].GetMean()
+            median[i] = getMedian(tot_calibrated[-1])
+            mode[i] = tot_calibrated[-1].GetBinCenter(tot_calibrated[-1].GetMaximumBin())
+            # Plot
             tot_calibrated[-1].Scale(1.0 / tot_calibrated[-1].Integral(), "HIST") 
             tot_calibrated[-1].SetTitle("ToT Calibrated for Different Voltages")
             tot_calibrated[-1].GetXaxis().SetTitle("ToT/ns")
@@ -49,11 +63,35 @@ def main(inputfiles):
             tot_calibrated[-1].SetLineColor(i+1)
             tot_calibrated[-1].Draw(f"HIST {opt}")
             file.Close()
+
         legend.Draw()
         if save_plots:
             c.SaveAs(f"Pictures/{folder}/tot_calibrated_for_different_kV_row_{irow}.{FORMAT}")
         if not omit_plots:
             input("Press Enter to continue...")
+        
+    if not omit_plots:
+        for i in range(6,10):
+            plt.plot(voltages_kV, mean[i], 'o-', label="Mean")
+            plt.plot(voltages_kV, median[i], 'o-', label="Median")
+            plt.plot(voltages_kV, mode[i], 'o-', label="Mode")
+        plt.xlabel('Voltage [V]')
+        plt.ylabel('ToT/ns')
+        plt.title('Voltage vs ToT')
+        plt.legend()
+        plt.show()
+        input("Press Enter to continue...")
+
+
+
+def getMedian(histo1):
+    numBins = histo1.GetXaxis().GetNbins()
+    y = np.array([])
+    for i in range(numBins):
+        x = histo1.GetBinCenter(i)
+        for ix in range(int(histo1.GetBinContent(i))):
+            y = np.append(y, x)
+    return np.median(y)
 
 if __name__ == '__main__':
     main()
--- plot_rootfiles.py
+++ plot_rootfiles.py
@@ -9,126 +9,145 @@ omit_plots = False
 ROOT.gROOT.SetBatch(omit_plots)
 ROOT.gStyle.SetOptStat(111111)
 
+def plot_hit_map(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("col:row>>hit_map(16,0,16,16,0,16)", cut,"colz")
+    hit_map = ROOT.gDirectory.Get("hit_map")
+    hit_map.SetTitle(title)
+    hit_map.GetXaxis().SetTitle("Row")
+    hit_map.GetYaxis().SetTitle("Column")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")      
+    
+def plot_tot_code(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("tot_code>>tot_code(512,0,511)", cut,"")
+    tot_code = ROOT.gDirectory.Get("tot_code")
+    # tot_code.SetTitle(title)
+    tot_code.GetXaxis().SetTitle("ToT/code")
+    tot_code.GetYaxis().SetTitle("Entries")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+
+def plot_tot_calibrated(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("tot_cal>>tot_calibrated(200,0,10)", cut,"")
+    tot_calibrated = ROOT.gDirectory.Get("tot_calibrated")
+    tot_calibrated.SetTitle(title)
+    tot_calibrated.GetXaxis().SetTitle("ToT/ns")
+    tot_calibrated.GetYaxis().SetTitle("Entries")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+
+def plot_tot_calibrated_full(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("tot_cal>>tot_calibrated(500,0,3000)", cut,"")
+    tot_calibrated = ROOT.gDirectory.Get("tot_calibrated")
+    tot_calibrated.SetTitle(title)
+    tot_calibrated.GetXaxis().SetTitle("ToT/ns")
+    tot_calibrated.GetYaxis().SetTitle("Entries")
+    c.SetLogy()
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+def plot_toa_code(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("toa_code>>toa_code(1025,0,1024)", cut,"")
+    toa_code = ROOT.gDirectory.Get("toa_code")
+    toa_code.SetTitle(title)
+    toa_code.GetXaxis().SetTitle("ToA/code")
+    toa_code.GetYaxis().SetTitle("Entries")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+
+def plot_toa_calibrated(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("toa_cal>>toa_calibrated(500,0,14)", cut,"")
+    toa_calibrated = ROOT.gDirectory.Get("toa_calibrated")
+    toa_calibrated.SetTitle(title)
+    toa_calibrated.GetXaxis().SetTitle("ToA/ns")
+    toa_calibrated.GetYaxis().SetTitle("Entries")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+
+def plot_cal(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("cal>>cal(1024,1,1024)", cut,"")
+    cal = ROOT.gDirectory.Get("cal")
+    cal.SetTitle(title)
+    cal.GetXaxis().SetTitle("Calibration")
+    cal.GetYaxis().SetTitle("Entries")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+    max_bin = cal.GetMaximumBin()
+    return max_bin
+
+def plot_tot_toa(file, canvas_name, title, file_name, folder, cut=""):
+    c = ROOT.TCanvas(canvas_name, canvas_name)
+    file.Hits.Draw("tot_cal:toa_cal>>tot_toa(200,0,10,500,0,14)", cut,"colz")
+    tot_toa = ROOT.gDirectory.Get("tot_toa")
+    tot_toa.SetTitle(title)
+    tot_toa.GetXaxis().SetTitle("ToT/ns")
+    tot_toa.GetYaxis().SetTitle("ToA/ns")
+    c.Draw()
+    if save_plots:
+        c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
+    if not omit_plots:
+        input("Press Enter to continue...")
+
 
 @click.command()
 @click.argument('inputfiles', nargs=-1)
 def main(inputfiles):
     # Directory to store the output plots
+    folder = ""
     if save_plots:
         input_name = inputfile.split('/')
         input_name[-1] = input_name[-1].split('.')[0]
         folder = "/".join(input_name[1:-1])
-        os.makedirs(f"Pictures/{folder}", exist_ok=True)
-    # Functions to do the plots
-    def plot_hit_map(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("col:row>>hit_map(16,0,16,16,0,16)", cut,"colz")
-        hit_map = ROOT.gDirectory.Get("hit_map")
-        hit_map.SetTitle(title)
-        hit_map.GetXaxis().SetTitle("Row")
-        hit_map.GetYaxis().SetTitle("Column")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")      
-        
-    def plot_tot_code(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("tot_code>>tot_code(512,0,511)", cut,"")
-        tot_code = ROOT.gDirectory.Get("tot_code")
-        # tot_code.SetTitle(title)
-        tot_code.GetXaxis().SetTitle("ToT/code")
-        tot_code.GetYaxis().SetTitle("Entries")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
-
-    def plot_tot_calibrated(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("tot_cal>>tot_calibrated(250,0,25)", cut,"")
-        tot_calibrated = ROOT.gDirectory.Get("tot_calibrated")
-        tot_calibrated.SetTitle(title)
-        tot_calibrated.GetXaxis().SetTitle("ToT/ns")
-        tot_calibrated.GetYaxis().SetTitle("Entries")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
-
-    def plot_toa_code(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("toa_code>>toa_code(1025,0,1024)", cut,"")
-        toa_code = ROOT.gDirectory.Get("toa_code")
-        toa_code.SetTitle(title)
-        toa_code.GetXaxis().SetTitle("ToA/code")
-        toa_code.GetYaxis().SetTitle("Entries")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
-    
-    def plot_toa_calibrated(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("toa_cal>>toa_calibrated(500,0,14)", cut,"")
-        toa_calibrated = ROOT.gDirectory.Get("toa_calibrated")
-        toa_calibrated.SetTitle(title)
-        toa_calibrated.GetXaxis().SetTitle("ToA/ns")
-        toa_calibrated.GetYaxis().SetTitle("Entries")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
-
-    def plot_cal(canvas_name, title, file_name, cut=""):
-        c = ROOT.TCanvas(canvas_name, canvas_name)
-        file.Hits.Draw("cal>>cal(1024,1,1024)", cut,"")
-        cal = ROOT.gDirectory.Get("cal")
-        cal.SetTitle(title)
-        cal.GetXaxis().SetTitle("Calibration")
-        cal.GetYaxis().SetTitle("Entries")
-        c.Draw()
-        if save_plots:
-            c.SaveAs(f"Pictures/{folder}/{file_name}{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
-        max_bin = cal.GetMaximumBin()
-        return max_bin
-    
+        os.makedirs(f"Pictures/{folder}", exist_ok=True)   
             
     for inputfile in inputfiles:
         file = ROOT.TFile(inputfile)
 
         # Plot functions
-        plot_hit_map("hit_map", f"Hit Map {inputfile}", f"{inputfile}_hit_map")
+        plot_hit_map(file, "hit_map", f"Hit Map {inputfile}", f"{inputfile}_hit_map", folder)
         max_bin = []
         for i in range(6,10):
-            max_bin.append(plot_cal(f"cal{i}", f"Calibration {i}", f"{inputfile}_cal_row_{i}", f"row == {i}"))
-        print(max_bin)
+            max_bin.append(plot_cal(file, f"cal{i}", f"Calibration {i}", f"{inputfile}_cal_row_{i}", folder, f"row == {i}"))
+        # for i in range(6,10):
+        #     plot_tot_code(file, f"tot_code{i}", f"ToT Code {i}", f"{inputfile}_tot_code_row_{i}", folder, f"row == {i}")
         for i in range(6,10):
-            plot_tot_code(f"tot_code{i}", f"ToT Code {i}", f"{inputfile}_tot_code_row_{i}", f"row == {i}")
+            plot_tot_calibrated(file, f"tot_calibrated{i}", f"ToT Calibrated {i}", f"{inputfile}_tot_calibrated_row_{i}", folder, f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
+            plot_tot_calibrated_full(file, f"tot_calibrated_full{i}", f"ToT Calibrated Full {i}", f"{inputfile}_tot_calibrated_full_row_{i}", folder, f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
+        # for i in range(6,10):    
+        #     plot_toa_code(file, f"toa_code{i}", f"ToA Code {i}", f"{inputfile}_toa_code_row_{i}", folder, f"abs(cal-{max_bin[i-6]})<1.5 && row == {i}")
+        for i in range(6,10):    
+            plot_toa_calibrated(file, f"toa_calibrated{i}", f"ToA Calibrated {i}", f"{inputfile}_toa_calibrated_row_{i}", folder, f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
         for i in range(6,10):
-            if len(max_bin) == 0:
-                plot_tot_calibrated(f"tot_calibrated{i}", f"ToT Calibrated {i}", f"{inputfile}_tot_calibrated_row_{i}", f"row == {i}")
-            else:
-                plot_tot_calibrated(f"tot_calibrated{i}", f"ToT Calibrated {i}", f"{inputfile}_tot_calibrated_row_{i}", f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
-        for i in range(6,10):    
-            plot_toa_code(f"toa_code{i}", f"ToA Code {i}", f"{inputfile}_toa_code_row_{i}", f"abs(cal-{max_bin[i-6]})<1.5 && row == {i}")
-        for i in range(6,10):    
-            plot_toa_calibrated(f"toa_calibrated{i}", f"ToA Calibrated {i}", f"{inputfile}_toa_calibrated_row_{i}", f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
+            plot_tot_toa(file, f"tot_toa{i}", f"ToT vs ToA {i}", f"{inputfile}_tot_toa_row_{i}", folder, f"abs(cal-{max_bin[i-6]})<2.5 && row == {i}")
 
         file.Close()
 
-
-
 if __name__ == '__main__':
     main()
-
-
-
-

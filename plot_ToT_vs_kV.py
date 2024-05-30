7d7f0ce938201426f0e6fab2f0b94665bf2ba409
antoniohockey13 <antoniogomezcarrera@gmail.com>
2024-05-30 13:46:46 +0200

    Mean, mode and median calculations

--- plot_ToT_vs_kV.py
+++ plot_ToT_vs_kV.py
@@ -2,6 +2,7 @@ import ROOT
 import click
 import os
 import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 # CONSTANTS
@@ -17,6 +18,11 @@ def get_cal(file, cut=""):
     max_bin = cal.GetMaximumBin()
     return max_bin
 
+def getMedian(histo1):
+    xq = np.array([0.5])
+    yq = np.zeros(1)
+    histo1.GetQuantiles(1, yq, xq)
+    return yq[0]
 
 @click.command()
 @click.argument('inputfiles', nargs=-1)
@@ -30,13 +36,15 @@ def main(inputfiles):
     mean = {}
     median = {}
     mode = {}
-    voltages_kV = [10,15,20,35,30,35]    
+    voltages_kV = [5,10,15,20,25,30,35]    
 
     for irow in range(6,10):
         c = ROOT.TCanvas(f"c{irow}", f"c{irow}")
         legend = ROOT.TLegend(0.1,0.7,0.48,0.9)
         tot_calibrated = []
-
+        mean[irow] = []
+        median[irow] = []
+        mode[irow] = []
         
         for i, inputfile in enumerate(reversed(inputfiles)):
             file = ROOT.TFile(inputfile)
@@ -51,47 +59,58 @@ def main(inputfiles):
             file.Hits.Project(f"tot_calibrated_{i}", "tot_cal", f"row == {irow} && cal-{max_bin_value}<2.5")
             tot_calibrated[-1].SetDirectory(0) 
             # Statistics values
-            mean[irow] = tot_calibrated[-1].GetMean()
-            median[i] = getMedian(tot_calibrated[-1])
-            mode[i] = tot_calibrated[-1].GetBinCenter(tot_calibrated[-1].GetMaximumBin())
+            mean[irow].append(tot_calibrated[-1].GetMean())
+            median[irow].append(getMedian(tot_calibrated[-1]))
+            mode[irow].append(tot_calibrated[-1].GetBinCenter(tot_calibrated[-1].GetMaximumBin()))
             # Plot
-            tot_calibrated[-1].Scale(1.0 / tot_calibrated[-1].Integral(), "HIST") 
             tot_calibrated[-1].SetTitle("ToT Calibrated for Different Voltages")
             tot_calibrated[-1].GetXaxis().SetTitle("ToT/ns")
             tot_calibrated[-1].GetYaxis().SetTitle("Entries")
             legend.AddEntry(tot_calibrated[-1], f"{inputfile}", "l")
             tot_calibrated[-1].SetLineColor(i+1)
-            tot_calibrated[-1].Draw(f"HIST {opt}")
+            tot_calibrated[-1].DrawNormalized(f"HIST {opt}")
             file.Close()
 
         legend.Draw()
         if save_plots:
             c.SaveAs(f"Pictures/{folder}/tot_calibrated_for_different_kV_row_{irow}.{FORMAT}")
-        if not omit_plots:
-            input("Press Enter to continue...")
+        # if not omit_plots:
+            # input("Press Enter to continue...")
         
-    if not omit_plots:
-        for i in range(6,10):
-            plt.plot(voltages_kV, mean[i], 'o-', label="Mean")
-            plt.plot(voltages_kV, median[i], 'o-', label="Median")
-            plt.plot(voltages_kV, mode[i], 'o-', label="Mode")
-        plt.xlabel('Voltage [V]')
-        plt.ylabel('ToT/ns')
-        plt.title('Voltage vs ToT')
-        plt.legend()
-        plt.show()
-        input("Press Enter to continue...")
+    for i in range(6,10):
+        plt.plot(voltages_kV, mean[i], 'o-', label=f"Col {i}")
+    plt.xlabel('Voltage [V]')
+    plt.ylabel('ToT/ns')
+    plt.title('Mean')
+    plt.legend()
+    plt.show()
+    if save_plots:
+        plt.savefig(f"Pictures/{folder}/Voltage_vs_ToT_Col_{i}.{FORMAT}")
+
+    for i in range(6,10):        
+        plt.plot(voltages_kV, median[i], 'o-', label=f"Col {i}")
+    plt.xlabel('Voltage [V]')
+    plt.ylabel('ToT/ns')
+    plt.title('Median')
+    plt.legend()
+    plt.show()
+    if save_plots:
+        plt.savefig(f"Pictures/{folder}/Voltage_vs_ToT_Col_{i}.{FORMAT}")
+
+    for i in range(6,10):
+        plt.plot(voltages_kV, mode[i], 'o-', label=f"Col {i}")
+    plt.xlabel('Voltage [V]')
+    plt.ylabel('ToT/ns')
+    plt.title('Mode')
+    plt.legend()
+    plt.show()
+    if save_plots:
+        plt.savefig(f"Pictures/{folder}/Voltage_vs_ToT_Col_{i}.{FORMAT}")
 
 
 
-def getMedian(histo1):
-    numBins = histo1.GetXaxis().GetNbins()
-    y = np.array([])
-    for i in range(numBins):
-        x = histo1.GetBinCenter(i)
-        for ix in range(int(histo1.GetBinContent(i))):
-            y = np.append(y, x)
-    return np.median(y)
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
